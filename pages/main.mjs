bea2cfe28b7296ec2bcf1d50b4d6d010c939b7fe
McbeEringi <46889548+McbeEringi@users.noreply.github.com>
2024-10-21 06:09:16 +0900

    -

--- pages/main.mjs
+++ pages/main.mjs
@@ -1,3 +1,7 @@
+import { readdir } from "node:fs/promises";
 Bun.write("build/index.html",`Hello Bun.write!`);
+(await readdir("src",{recursive:1})).forEach(x=>
+	Bun.write("build/"+x,Bun.file("src/"+x))
+);
 Bun.write("build/test/index.html",`trailing slash?`);
 Bun.write("build/404.html",`404dayo`);
