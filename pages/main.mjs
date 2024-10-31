3dc2a281831d282a1372bd1ec677ba36c9700e6d
McbeEringi <git@mcbeeringi.dev>
2024-11-01 01:22:16 +0900

    -

--- pages/main.mjs
+++ pages/main.mjs
@@ -2,6 +2,6 @@ import { readdir, mkdir, rmdir } from 'node:fs/promises';
 
 await rmdir('build',{recursive:1});await mkdir('build');
 (await readdir('assets',{recursive:1,withFileTypes:1})).forEach(x=>(x.isFile()||x.isSymbolicLink())&&(
-	x=(x.parentPath+'/').slice('assets/'.length)+x.name,
+	x=(x.parentPath+'/'+x.name).slice('assets/'.length),
 	Bun.write('build/'+x,Bun.file('assets/'+x))
 ));
