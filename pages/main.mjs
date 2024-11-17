3b2e6e72d6a1fe6dab3401926f5bffecf80b4d06
McbeEringi <git@mcbeeringi.dev>
2024-11-18 03:02:13 +0900

    -

--- pages/main.mjs
+++ pages/main.mjs
@@ -16,32 +16,32 @@ src=async({dir,url})=>(
 		await rmdir(odir,{recursive:1}),
 		await mkdir(odir),
 		(await readdir(idir,{recursive:1,withFileTypes:1})).reduce(async(a,x)=>(
-			a=await a,
+			await a,
 			x.isFile()&&x.name.slice(-4)=='.mjs'&&(
 				x.x=(await import('./'+[x.parentPath,x.name].join('/'))).default,
 				console.log([x.parentPath,x.name].join('/'),x.x),
 				x.x&&(f=>f(f))(
 					f=>(od,w)=>Object.entries(w).forEach(([i,y])=>(y instanceof Blob?Bun.write:f(f))([od,i].join('/'),y))
 				)([odir,x.parentPath.slice(idir.length+1)].join('/'),{[x.name.slice(0,-4)]:x.x})
-			),
-			a
+			)
 		),0)
 	),
-	// dev:async(idir='src')=>(
-	// 	Bun.serve({
-	// 		async fetch(req){return(
-	// 			console.log(req.url),
-	// 			req=await(await src({
-	// 				dir:idir,
-	// 				url:req.url
-	// 			}))
-	// 			.reduce(async(a,x)=>(a=await a,
-	// 				x=(await import('./'+[x.parentPath,x.name].join('/'))).default,
-	// 				a
-	// 			),null)
-	// 			,console.log(req),
-	// 			new Response(req)
-	// 		);},
-	// 	})
-	// )
+	dev:async(idir='src')=>(
+		Bun.serve({
+			async fetch(req){return(
+				console.log(req.url),
+				req=await(await src({
+					dir:idir,
+					url:req.url
+				}))
+				.reduce(async(a,x)=>(
+					a=await a,
+					x=(await import('./'+[x.parentPath,x.name].join('/'))).default,
+					a
+				),null),
+				console.log(req),
+				new Response(req)
+			);},
+		})
+	)
 })[Bun.argv.slice(2)[0]||'build']();
--- pages/src/test/index.html.mjs
+++ pages/src/test/index.html.mjs
@@ -1,4 +1,5 @@
 import{HTML as d,tmpl}from'../../formats/html.tmpl.mjs';
+
 export default tmpl({title:'test',depth:1},[
 	'trailing slash?'
 ]);
