f53f7a6db739cf76c077529e91652ac9f6b47a67
McbeEringi <git@mcbeeringi.dev>
2024-11-21 19:36:04 +0900

    -

--- pages/formats/png.mjs
+++ pages/formats/png.mjs
@@ -1,5 +1,3 @@
-import { readdir } from 'node:fs/promises';
-
 const
 unpng=w=>[{bin:[w.slice(0,8)]},...{[Symbol.iterator]:(
 	p=8
@@ -9,6 +7,9 @@ unpng=w=>[{bin:[w.slice(0,8)]},...{[Symbol.iterator]:(
 	w.slice(p,p+=v.length),
 	w.slice(p,p+=4),
 ],{value:v})})}],
-crc=(t=>(buf,crc=0)=>~buf.reduce((c,x)=>t[(c^x)&0xff]^(c>>>8),~crc))([...Array(256)].map((_,n)=>[...Array(8)].reduce(c=>(c&1)?0xedb88320^(c>>>1):c>>>1,n)));// https://www.rfc-editor.org/rfc/rfc1952
+ch=x=>((
+	crc=(t=>(buf,crc=0)=>~buf.reduce((c,x)=>t[(c^x)&0xff]^(c>>>8),~crc))([...Array(256)].map((_,n)=>[...Array(8)].reduce(c=>(c&1)?0xedb88320^(c>>>1):c>>>1,n))),// https://www.rfc-editor.org/rfc/rfc1952
+	be4=x=>[x>>>24&255,x>>>16&255,x>>>8&255,x>>>0&255],
+)=>new Uint8Array([...be4(x.length-4),...x,...be4(crc(x))]))()
 
-export{unpng,crc};
+export{unpng,ch};
--- pages/src/favicon.ico.mjs
+++ pages/src/favicon.ico.mjs
@@ -1,5 +1,12 @@
-import{unpng}from'../formats/png.mjs';
-console.log(
-	unpng(await Bun.file('assets/icon.png').bytes())
-);
-export default Bun.file('assets/icon.png');
+import{unpng,ch}from'../formats/png.mjs';
+
+export default new Blob([].concat(...unpng(await Bun.file('assets/icon.png').bytes()).map(x=>(
+	x.name=='PLTE'?[
+		ch([80,76,84,69,...[
+			0x00ffff,0x40ffff,0x80ffff,0xc0ffff,
+			0xff00ff,0xff40ff,0xff80ff,0xffc0ff,
+			0xffff00,0xffff40,0xffff80,0xffffc0,
+			0xffffff
+		].flatMap(x=>[x>>16&255,x>>8&255,x&255])])		
+	]:x.bin
+))))
