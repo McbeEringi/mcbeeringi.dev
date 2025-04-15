a3fb6924c8c21705290325c42df6e9e79bc1f099
McbeEringi <git@mcbeeringi.dev>
2025-04-15 17:56:09 +0900

    -

---
+++ _pages/README.md
@@ -0,0 +1,10 @@
+```bash
+make
+```
+
+src->dst
+
+execute:`run-*`
+- deep -> shallow
+- 0 -> 9, A -> Z, a -> z (ASCII)
+escape:`esc-*`
---
+++ _pages/src/totp/sw.js
@@ -0,0 +1,50 @@
+//https://developer.mozilla.org/ja/docs/Web/Progressive_web_apps/Offline_Service_workers
+//https://developers.google.com/web/fundamentals/primers/service-workers?hl=ja
+const
+d=[
+	{
+		id:'totp_index_2412021',
+		init:['index.html']
+	},
+	{
+		id:'totp_img_2412021',
+		init:['img/icon.svg','img/icon.png']
+	},
+	{
+		id:'totp_misc_0',
+		misc:true
+	},
+];
+
+
+Object.entries({
+	install:e=>Promise.all(d.map(async x=>x.init&&(await caches.open(x.id)).addAll(x.init))),
+	activate:async e=>await Promise.all((await caches.keys()).map(async x=>d.some(_=>_.id==x)||await caches.delete(x))),
+	fetch:async e=>await(async(r,x)=>x?(x.slice(0,6)=='bytes='&&(x=x.slice(6).split(/,\s*/)).length==1?(
+		console.log(`RANGE ${x}`),
+		x=x.split('-'),
+		r=await r.arrayBuffer(),
+		x=[
+			_=>x.map(x=>+x),
+			_=>[+x[0],r.byteLength-1],
+			_=>[-x[1],r.byteLength-1],
+			_=>[0,r.byteLength-1]
+		][!x[0]<<1|!x[1]](),
+		new Response(r.slice(x[0],x[1]+1),{
+			status:206,statusText:'Partial Content',
+			headers:{
+				'Content-Type':e.request.headers.get('content-type'),
+				'Content-Range':`bytes ${x.join('-')}/${r.byteLength}`
+			}
+		})
+	):Response.error()):r)(
+		// (await caches.match(e.request.url,{ignoreSearch:1}))||(([r,c])=>(c.put(e.request.url,r.clone()),r))(await Promise.all([fetch(e.request.url),caches.open(d.find(x=>x.misc).id)])),
+		await fetch(e.request.url).then(
+			async r=>(await(await caches.open(d.find(x=>x.misc).id)).put(e.request.url,r.clone()),r),
+			async _=>(await caches.match(e.request.url,{ignoreSearch:1}))
+		),
+		e.request.headers.get('range')
+	)
+}).forEach(([i,x])=>self.addEventListener(i,e=>(console.log(i,e),e[i=='fetch'?'respondWith':'waitUntil'](x(e)))));
+
+console.log('hello from sw');
