d6efc8fab34efb233a2e8f1848f9b92c2ec1f3df
McbeEringi <git@mcbeeringi.dev>
2025-04-30 09:35:45 +0900

    -

--- _pages/src/img/run-10-rasterize.mjs
+++ _pages/src/img/run-10-rasterize.mjs
@@ -1,11 +1,8 @@
 #!/usr/bin/env -S bun
+import{Glob}from'bun';
 import{svg2png}from'../src/svg2png.mjs';
 
-const
-main=async n=>await Bun.write(
+new Glob('*.svg').scanSync('.').forEach(async n=>await Bun.write(
 	`${n}.png`,
 	svg2png(await Bun.file(n).text(),console.log(n))
-);
-
-main('icon.svg');
-main('icon_hat.svg');
+));
--- _pages/src/run-10-html.mjs
+++ _pages/src/run-10-html.mjs
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bun
 import{Glob}from'bun';
 
-new Glob('**/*.html').scanSync('.').forEach(async n=>Bun.write(n,(x=>(
+new Glob('**/*.html').scanSync('.').forEach(async n=>await Bun.write(n,(x=>(
 	x
 		.replace(/<html>/,'<html lang="ja">')
 		.replace(/(\t*)<head>\n?/,`$1<head>\n$1\t<meta charset="utf-8">\n$1\t<meta name="viewport" content="width=device-width,initial-scale=1">\n`)
-))(await Bun.file(n).text())))
+))(await Bun.file(n).text(),console.log(n))))
