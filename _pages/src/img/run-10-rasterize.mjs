dc0707ff3e1467dbc0bd05d133400d3d7e3e11b8
McbeEringi <git@mcbeeringi.dev>
2025-06-14 10:28:48 +0900

    -

--- _pages/src/img/run-10-rasterize.mjs
+++ _pages/src/img/run-10-rasterize.mjs
@@ -1,8 +1,19 @@
 #!/usr/bin/env -S bun
-import{Glob}from'bun';
-import{svg2png}from'../src/svg2png.mjs';
+import{Glob,$}from'bun';
+// import{svg2png}from'../src/svg2png.mjs';
+import{mkpltepng}from'../src/mkpltepng.mjs';
 
-new Glob('*.svg').scanSync('.').forEach(async n=>await Bun.write(
-	`${n}.png`,
-	svg2png(await Bun.file(n).text(),console.log(n))
+await Bun.write('plte.png',mkpltepng([
+	0xffffffff,
+	0x66ccaaff,
+	0x888888ff,
+	0x000000ff,
+]));
+await new Promise(f=>setTimeout(f,1000));
+new Glob('*.svg').scanSync('.').forEach(async n=>(
+	// await Bun.write(
+	// 	`${n}.png`,
+	// 	svg2png(await Bun.file(n).text(),console.log(n))
+	// )
+	$`magick ${n} +dither -remap plte.png ${n}.png`
 ));
