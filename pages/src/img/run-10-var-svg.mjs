0e68d86c4164392cd5b67a252df29c39ea4bdee5
McbeEringi <git@mcbeeringi.dev>
2025-06-17 06:09:38 +0900

    -

--- pages/src/img/run-10-var-svg.mjs
+++ pages/src/img/run-10-var-svg.mjs
@@ -1,11 +1,9 @@
 #!/usr/bin/env -S bun
 const
-w=await Bun.file('icon+.svg').text(),
-col=p=>w.replace(/.*?@media.*?\n/,'').replace('var(--s)',p.fg).replace('class="b"',`fill="${p.bg}"`),
+w=await Bun.file('icon.svg').text(),
+wi=w.replace(/(?<=#)[0-9a-f]{2,}/g,x=>({'fff':'222','000':'fff'}[x]||x)),
 tr=w=>w.replace(/.*?<path.+?\/>\n/,'');
 
-await Bun.write('icon.svg',col({fg:'#000',bg:'#fff'}));
-await Bun.write('icon!.svg',col({fg:'#fff',bg:'#222'}));
-await Bun.write('icon_.svg',tr(col({fg:'#000'})));
-await Bun.write('icon!_.svg',tr(col({fg:'#fff'})));
-await Bun.write('icon+_.svg',tr(w));
+await Bun.write('icon!.svg',wi);
+await Bun.write('icon_.svg',tr(w));
+await Bun.write('icon!_.svg',tr(wi));
