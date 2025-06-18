f68ac359b443f1198a4ae7705683c146ec9d9b2c
McbeEringi <git@mcbeeringi.dev>
2025-06-19 01:58:18 +0900

    -

--- pages/src/img/run-10-var-svg.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S bun
-const
-w=await Bun.file('icon.svg').text(),
-wi=w.replace(/(?<=#)[0-9a-f]{2,}/g,x=>({'fff':'222','000':'fff'}[x]||x)),
-tr=w=>w.replace(/.*?<path.+?\/>\n/,'');
-
-await Bun.write('icon!.svg',wi);
-await Bun.write('icon_.svg',tr(w));
-await Bun.write('icon!_.svg',tr(wi));
